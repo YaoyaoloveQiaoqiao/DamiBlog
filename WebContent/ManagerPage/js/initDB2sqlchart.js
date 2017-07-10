d38201e5560c1e6ba82b03a6dc7a2096dbb5cd11
yaoyaoloveqiaoqiao <632212519@qq.com>
2017-07-10 23:35:18 +0800

    aa

--- WebContent/ManagerPage/js/initDB2sqlchart.js
+++
@@ -1,5 +0,0 @@
-var sqlchart = initNomalEChart([[0,0]],'sqlchart',"","","line","category","单位/个","SQL吞吐量和响应时间",true);
-function sqlchartInit(){
-	refreshNomalEChart("../DBMon_Conn_Chart_SQLThroughput?dbid="+GetParam("dbid")+"&period=real&interval=5",sqlchart,undefined,undefined,undefined,"selectedNumber");
-	intervalId2 = intervalRefreshEchart("../DBMon_Conn_Chart_SQLThroughput?dbid="+GetParam("dbid")+"&period=real&interval=5",sqlchart,undefined,undefined,undefined,"selectedNumber");
-}
